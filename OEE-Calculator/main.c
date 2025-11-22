aacdfe33b67f370938ce2a08567b64faf243fe05
Thejan-Rodrigo <thejanpandula2003@gmail.com>
2025-11-22 16:50:34 +0530

    feat: create username_exists finction

--- OEE-Calculator/main.c
+++ OEE-Calculator/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define USERDATA "userData.txt"
+
+// Function to check if username already exists
+int username_exists(const char *username) {
+    FILE *file = fopen(USERDATA, "r");
+    if (file == NULL) {
+        return 2; // File doesn't exist, no usernames yet
+    }
+
+    char stored_username[100], stored_password[100];
+    while (fscanf(file, "%s %s", stored_username, stored_password) != EOF) {
+        if (strcmp(stored_username, username) == 0) {
+            fclose(file);
+            return 1; // Username exists
+        }
+    }
+
+    fclose(file);
+    return 0; // Username does not exist
+}
+
 int main()
 {
     while(1){
@@ -22,11 +43,30 @@ int main()
             printf("Login Page!");
             break;
          case 2:
-            printf("Create Account\n");
-            printf("------------------\n");
-            printf("Enter username: ");
-            scanf("%s", &username);
-            printf("user name is %s \n", username);
+            while(1){
+                printf("Create Account\n");
+                printf("------------------\n");
+                printf("Enter username: ");
+                scanf("%s", &username);
+                int isExists = username_exists(username);
+                switch(isExists){
+                  case 0:
+                      break;
+                  case 1:
+                      printf(" \n");
+                      printf("User Name Exists! Please User another User name\n");
+                      printf(" \n");
+                      continue;
+                  case 2:
+                      printf(" \n");
+                      printf("Can not find the for the path %s . \n", USERDATA);
+                      printf(" \n");
+                      break;
+                }
+                break;
+            }
+
+
             break;
          default:
             printf(" \n");
